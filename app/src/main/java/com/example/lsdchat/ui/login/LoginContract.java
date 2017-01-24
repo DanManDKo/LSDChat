0cff7a427fa824b169266575bde1439cae4e864a
serj <fteroy@gmail.com>
2017-01-25 01:05:49 +0200

    implement mvp contract

---
+++ app/src/main/java/com/example/lsdchat/ui/login/LoginContract.java
@@ -0,0 +1,42 @@
+package com.example.lsdchat.ui.login;
+
+
+
+import com.example.lsdchat.base.BaseMvpPresenter;
+import com.example.lsdchat.base.BaseMvpView;
+import com.quickblox.users.model.QBUser;
+
+
+public interface LoginContract {
+    interface Presented extends BaseMvpPresenter<View> {
+
+        void onDestroy();
+
+        void validateCredentials(String email, String password);
+
+    }
+
+    interface View extends BaseMvpView {
+        void showProgressBar();
+        void hideProgressBar();
+
+        void setEmailError();
+        void setPasswordError();
+
+        void hideEmailError();
+        void hidePasswordError();
+
+        void setLoginButtonEnabled(boolean enabled);
+
+        void navigateToRegistration();
+        void navigateToForgotPassword();
+        void navigateToMainScreen();
+
+        boolean isKeepSignIn();
+        boolean isValidPassword(String password);
+        boolean isValidEmail(String email);
+
+
+    }
+
+}
