0991ad63a23b5d8a1c4988bb720432a259158f4a
serj <fteroy@gmail.com>
2017-01-27 19:35:46 +0200

    edit structure api; use rx

--- app/src/main/java/com/example/lsdchat/util/Network.java
+++ app/src/main/java/com/example/lsdchat/util/Network.java
@@ -4,9 +4,6 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
-import com.example.lsdchat.model.SessionRequestBody;
-import com.example.lsdchat.model.SessionResponse;
-
 
 
 /**
