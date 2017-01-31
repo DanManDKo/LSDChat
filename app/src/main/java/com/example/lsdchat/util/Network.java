065b57bf5f7f923a8b22152c50298c5a95a1a680
serj <fteroy@gmail.com>
2017-01-31 02:23:46 +0200

    add errors code, dialog error

--- app/src/main/java/com/example/lsdchat/util/Network.java
+++ app/src/main/java/com/example/lsdchat/util/Network.java
@@ -1,8 +1,11 @@
 package com.example.lsdchat.util;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+
+import com.example.lsdchat.R;
 
 
 /**
@@ -21,5 +24,13 @@ public class Network {
         return isAvailable;
     }
 
+    public static void showErrorConnectDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(context.getString(R.string.network_is_absent))
+                .setMessage(context.getString(R.string.check_connection))
+                .setPositiveButton(context.getString(R.string.retry), (dialog, which) -> dialog.dismiss())
+                .setCancelable(false).create().show();
+
+    }
 
 }
