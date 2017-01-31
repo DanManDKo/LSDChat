4ede122df6d4a5230249bf0b10b14e5ce27cc40e
serj <fteroy@gmail.com>
2017-01-31 16:32:54 +0200

    edit name

---
+++ app/src/main/java/com/example/lsdchat/api/registration/response/RegistrationCreateFileBlobObjectAccess.java
@@ -0,0 +1,56 @@
+package com.example.lsdchat.api.registration.response;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RegistrationCreateFileBlobObjectAccess {
+    @SerializedName("blob_id")
+    private Integer blobId;
+    @SerializedName("expires")
+    private String expires;
+    @SerializedName("id")
+    private Integer id;
+    @SerializedName("object_access_type")
+    private String objectAccessType;
+    @SerializedName("params")
+    private String params;
+
+    public Integer getBlobId() {
+        return blobId;
+    }
+
+    public void setBlobId(Integer blobId) {
+        this.blobId = blobId;
+    }
+
+    public String getExpires() {
+        return expires;
+    }
+
+    public void setExpires(String expires) {
+        this.expires = expires;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getObjectAccessType() {
+        return objectAccessType;
+    }
+
+    public void setObjectAccessType(String objectAccessType) {
+        this.objectAccessType = objectAccessType;
+    }
+
+    public String getParams() {
+        return params;
+    }
+
+    public void setParams(String params) {
+        this.params = params;
+    }
+}
--- app/src/test/java/com/example/lsdchat/ExampleUnitTest.java
+++ app/src/test/java/com/example/lsdchat/ExampleUnitTest.java
@@ -1,8 +1,12 @@
 package com.example.lsdchat;
 
-import org.junit.Test;
+import com.example.lsdchat.ui.login.LoginActivity;
+import com.example.lsdchat.ui.login.LoginPresenter;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +14,20 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    @Mock
+    LoginActivity loginActivity;
+    private LoginPresenter mLoginPresenter;
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void submitButtonTest() {
+//       mLoginPresenter.
+    }
+
+
 }
