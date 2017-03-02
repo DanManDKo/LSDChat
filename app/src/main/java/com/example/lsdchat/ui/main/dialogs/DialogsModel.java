866cf44c61d3750c007e2fd3aebfd3232ffc961a
serj <fteroy@gmail.com>
2017-03-03 01:27:21 +0200

    resolve conflict

--- app/src/main/java/com/example/lsdchat/ui/main/dialogs/DialogsModel.java
+++ app/src/main/java/com/example/lsdchat/ui/main/dialogs/DialogsModel.java
@@ -1,8 +1,7 @@
 package com.example.lsdchat.ui.main.dialogs;
 
 
-import com.example.lsdchat.api.dialog.DialogService;
-import com.example.lsdchat.api.dialog.response.DialogsResponse;
+import com.example.lsdchat.App;
 import com.example.lsdchat.api.login.service.LoginService;
 import com.example.lsdchat.manager.DataManager;
 import com.example.lsdchat.model.User;
@@ -14,24 +13,15 @@ import rx.schedulers.Schedulers;
 public class DialogsModel implements DialogsContract.Model {
     private DataManager mDataManager;
     private LoginService mLoginService;
-    private DialogService mDialogService;
 
-    public DialogsModel(DataManager dataManager, LoginService loginService, DialogService dialogService) {
-        mLoginService = loginService;
-        mDataManager = dataManager;
-        mDialogService = dialogService;
+    public DialogsModel() {
+        mLoginService = App.getApiManager().getLoginService();
+        mDataManager = App.getDataManager();
     }
 
     @Override
     public User getCurrentUser() {
         return mDataManager.getUser();
-    }
-
-    @Override
-    public Observable<DialogsResponse> getDialogs(String token) {
-        return mDialogService.getDialog(token)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
     }
 
 
