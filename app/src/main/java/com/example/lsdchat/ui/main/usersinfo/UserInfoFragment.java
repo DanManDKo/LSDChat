a846ae4569eeeed0757d38d6b5f653b53a0a9671
serj <fteroy@gmail.com>
2017-03-04 23:03:53 +0200

    add logic click on relative

--- app/src/main/java/com/example/lsdchat/ui/main/usersinfo/UserInfoFragment.java
+++ app/src/main/java/com/example/lsdchat/ui/main/usersinfo/UserInfoFragment.java
@@ -1,9 +1,15 @@
 package com.example.lsdchat.ui.main.usersinfo;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.Toolbar;
+import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,26 +17,27 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.example.lsdchat.App;
 import com.example.lsdchat.R;
 import com.example.lsdchat.api.login.model.LoginUser;
 import com.example.lsdchat.ui.main.fragment.BaseFragment;
+import com.example.lsdchat.util.ErrorsCode;
 
-import java.io.Serializable;
 
-
-public class UserInfoFragment extends BaseFragment implements UserInfoContract.View{
+public class UserInfoFragment extends BaseFragment implements UserInfoContract.View {
+    private static final String LIST = "list";
     private FloatingActionButton mFabMessage;
     private TextView mEmail;
     private TextView mPhone;
     private TextView mWebsite;
     private Toolbar mToolbar;
-    private static final String LIST= "list";
     private LoginUser mLoginUser;
     private ImageView mImageUser;
     private RelativeLayout mRlPhone;
     private RelativeLayout mRlEmail;
     private RelativeLayout mRlWebsite;
     private UserInfoContract.Presenter mPresenter;
+    private PackageManager mPackageManager;
 
 
     public UserInfoFragment() {
@@ -39,8 +46,8 @@ public class UserInfoFragment extends BaseFragment implements UserInfoContract.V
 
     public UserInfoFragment newInstance(LoginUser user) {
         UserInfoFragment userInfoFragment = new UserInfoFragment();
-        Bundle bundle  = new Bundle();
-        bundle.putSerializable(LIST,(Serializable) user);
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(LIST, user);
         userInfoFragment.setArguments(bundle);
         return userInfoFragment;
     }
@@ -55,17 +62,27 @@ public class UserInfoFragment extends BaseFragment implements UserInfoContract.V
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_users_info, container, false);
-        mPresenter = new UserInfoPresenter(this);
-        mLoginUser = (LoginUser) getArguments().getSerializable(LIST);
+        mPresenter = new UserInfoPresenter(this, App.getSharedPreferencesManager(getActivity()));
+        mPackageManager = getActivity().getPackageManager();
+        mLoginUser = getArguments().getParcelable(LIST);
         initView(view);
 
         mEmail.setText(mLoginUser.getEmail());
         mPhone.setText(mLoginUser.getPhone());
         mWebsite.setText(mLoginUser.getWebsite());
+        mPresenter.setImageView(mImageUser, mLoginUser.getBlobId());
 
+        onClick();
 
 
         return view;
+    }
+
+    private void onClick() {
+        mPresenter.setOnClickListenerFab(mFabMessage, mLoginUser);
+        mPresenter.setOnClickListenerRlEmail(mRlEmail, mLoginUser.getEmail());
+        mPresenter.setOnClickListenerRlPhone(mRlPhone, mLoginUser.getPhone());
+        mPresenter.setOnClickListenerRlWeb(mRlWebsite, mLoginUser.getWebsite());
     }
 
     private void initView(View view) {
@@ -79,9 +96,45 @@ public class UserInfoFragment extends BaseFragment implements UserInfoContract.V
         mRlPhone = (RelativeLayout) view.findViewById(R.id.user_info_phone_rl);
         mRlWebsite = (RelativeLayout) view.findViewById(R.id.user_info_website_rl);
 
-        initToolbar(mToolbar,mLoginUser.getFullName());
+        initToolbar(mToolbar, mLoginUser.getFullName());
         mToolbar.setNavigationOnClickListener(v -> getActivity().onBackPressed());
     }
 
 
+    @Override
+    public void navigateDial(String phone) {
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL, Uri.fromParts(
+                "tel", phone, null));
+        if (mPackageManager.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
+            if (((TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE))
+                    .getSimState() == TelephonyManager.SIM_STATE_READY) {
+                if (Settings.Global.getInt(getContext().getContentResolver(),
+                        Settings.Global.AIRPLANE_MODE_ON, 0) == 0) {
+                    startActivity(phoneIntent);
+                    return;
+                }
+            }
+        } else {
+            ErrorsCode.showErrorDialog(getActivity(), getString(R.string.user_info_error_phone));
+        }
+    }
+
+    @Override
+    public void navigateSendEmail(String email) {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto", email, null));
+        if (emailIntent.resolveActivity(mPackageManager) != null) {
+            startActivity(emailIntent);
+        } else {
+            ErrorsCode.showErrorDialog(getActivity(), getString(R.string.user_info_error_email));
+        }
+    }
+
+    @Override
+    public void navigateWeb(String website) {
+        Intent websiteIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(website));
+        if (websiteIntent.resolveActivity(mPackageManager) != null) {
+            startActivity(websiteIntent);
+        }
+    }
 }
