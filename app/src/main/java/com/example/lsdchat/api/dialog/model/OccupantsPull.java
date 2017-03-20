ddf8e9b3eade384e9f97d6b461bde6b19f2dc865
Sergey <fteroy@gmial.com>
2017-03-21 00:23:28 +0200

    edit editchat logic

--- app/src/main/java/com/example/lsdchat/api/dialog/model/OccupantsPull.java
+++ app/src/main/java/com/example/lsdchat/api/dialog/model/OccupantsPull.java
@@ -4,12 +4,13 @@ package com.example.lsdchat.api.dialog.model;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
+import java.util.Set;
 
 public class OccupantsPull {
     @SerializedName("occupants_ids")
-    private List<Integer> pullAll;
+    private Set<Integer> pullAll;
 
-    public OccupantsPull(List<Integer> pullAll) {
+    public OccupantsPull(Set<Integer> pullAll) {
         this.pullAll = pullAll;
     }
 }
--- app/src/main/java/com/example/lsdchat/api/dialog/model/OccupantsPush.java
+++ app/src/main/java/com/example/lsdchat/api/dialog/model/OccupantsPush.java
@@ -3,12 +3,13 @@ package com.example.lsdchat.api.dialog.model;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
+import java.util.Set;
 
 public class OccupantsPush {
     @SerializedName("occupants_ids")
-    private List<Integer> pushAll;
+    private Set<Integer> pushAll;
 
-    public OccupantsPush(List<Integer> pushAll) {
+    public OccupantsPush(Set<Integer> pushAll) {
         this.pushAll = pushAll;
     }
 }
